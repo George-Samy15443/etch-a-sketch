a4984bab3bec7fdd68814eb4e0389e24eef9e8fe
George-Samy <91060888+George-Samy15443@users.noreply.github.com>
2026-08-29 14:02:44 +0300

    added button to change grid size

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 const container = document.querySelector('.grid-container')
-console.log(container);
+const btn = document.querySelector('button')
 
-for (let i  = 0; i < 256; i++) {
-    const square = document.createElement('div')
-    square.classList.add('grid-item')
-    container.appendChild(square)
-    // square.addEventListener('mouseover', (e) => {
-    //     e.target.style.backgroundColor = 'red'
-    // })
+function makeGrid(gridSize){
+    container.textContent = ''
+    for (let i  = 0; i < Math.pow(gridSize, 2); i++) {
+        const square = document.createElement('div')
+        square.classList.add('grid-item')
+        square.style.width = `calc((100% - (${gridSize - 1} * 2px)) / ${gridSize})`
+        square.style.height = `calc((100% - (${gridSize - 1} * 2px)) / ${gridSize})`
+        container.appendChild(square)
+    }
 }
+
+makeGrid(16)
+
+btn.addEventListener('click', () => {
+    let newGridSize = Number(prompt('Enter new grid size. Maximum is 100, Minimum is 16:'))
+    while(newGridSize > 100 || newGridSize < 16){
+        if(newGridSize > 100)
+            newGridSize = +prompt('Too high, Maximum is 100, Minimum is 16. Please Enter again:');
+        else if(newGridSize < 16)
+            newGridSize = +prompt('Too low, Maximum is 100, Minimum is 16. Please Enter again:');
+    
+    }    
+    makeGrid(newGridSize)
+})
+
